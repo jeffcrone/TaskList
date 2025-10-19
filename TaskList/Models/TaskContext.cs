c383794db9ca4f9dae0605851ffcf1c19194fc65
jeffcrone <81781741+jeffcrone@users.noreply.github.com>
2025-10-18 20:05:10 -0500

    Added a dbcontext class.

---
+++ TaskList/Models/TaskContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskList.Models
+{
+	public class TaskContext : DbContext
+	{
+		public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+		{
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite("Data Source=tasklist.db");
+		}
+
+		public DbSet<TaskItem> TaskItems { get; set; } = null!;
+	}
+}
