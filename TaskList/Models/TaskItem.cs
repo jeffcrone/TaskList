4a123e4083d6de2239bdb4b9229f4e6579533bfa
jeffcrone <81781741+jeffcrone@users.noreply.github.com>
2025-10-18 19:51:55 -0500

    Added a TaskItem model class.

---
+++ TaskList/Models/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskList.Models
+{
+	public class TaskItem
+	{
+		public int Id { get; set; }
+		public required string Title { get; set; }
+		public required string Description { get; set; }
+		public bool IsComplete { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}
